67ca8734fe5fdde65a94cc9cfbcfeac314113dc7
pinterbela <pinter.bela@udinfopark.hu>
2025-01-16 13:40:21 +0100

    refactor - move class

--- src/main/java/hu/udinfopark/jpa/optimistic/JpaOptimisticLockingApplication.java
+++ src/main/java/hu/udinfopark/jpa/optimistic/JpaOptimisticLockingApplication.java
@@ -1,4 +1,4 @@
-package hu.udinfopark.jpa.optimistic.test;
+package hu.udinfopark.jpa.optimistic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
