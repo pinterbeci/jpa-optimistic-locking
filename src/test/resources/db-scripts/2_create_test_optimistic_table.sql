05d1dae4042e61f174bf5cf434a244483da52446
pinterbela <pinter.bela@udinfopark.hu>
2025-01-16 13:38:41 +0100

    add banner

---
+++ src/test/resources/db-scripts/2_create_test_optimistic_table.sql
@@ -0,0 +1,13 @@
+
+create table if not exists optimistic
+(
+    id            varchar      not null unique default uuid_generate_v4(),
+    description   varchar,
+    is_deleted    boolean      default false,
+    version       numeric      default 0,
+    modifier_id   varchar      not null,
+    last_modified timestamp(6) not null default current_timestamp(6),
+    creator_id    varchar      not null,
+    created_at    timestamp(6) not null,
+    constraint pk_address_entity primary key (id)
+ );
